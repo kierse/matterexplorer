60e9a9e1dbe2f32936e5a6e59d5df8b1b5a9a0d0
Kieran Elliott <kierse@pissiphany.com>
2015-10-03 00:17:05 -0600

    touched up CREATE TABLE

--- app/src/main/java/com/pissiphany/matterexplorer/db/DatabaseHelper.java
+++ app/src/main/java/com/pissiphany/matterexplorer/db/DatabaseHelper.java
@@ -10,23 +10,23 @@ import com.pissiphany.matterexplorer.provider.contract.MatterContract;
  * Created by kierse on 15-08-23.
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "matter_explorer";
+    private static final String DATABASE_NAME = "matter_explorer.db";
     private static final int DATABASE_VERSION = 1;
 
     private static final String DATABASE_CREATE = "" +
             "CREATE TABLE " + MatterContract.TABLE + " "
                 + "("
-                + MatterContract.Columns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + MatterContract.Columns.ID + " INTEGER UNIQUE,"
-                + MatterContract.Columns.UPDATED_AT + " TEXT,"
-                + MatterContract.Columns.CREATED_AT + " TEXT,"
-                + MatterContract.Columns.DESCRIPTION + " TEXT,"
-                + MatterContract.Columns.DISPLAY_NUMBER + " TEXT,"
-                + MatterContract.Columns.STATUS + " TEXT,"
-                + MatterContract.Columns.PENDING_DATE + " TEXT,"
-                + MatterContract.Columns.OPEN_DATE + " TEXT,"
-                + MatterContract.Columns.CLOSE_DATE + " TEXT,"
-                + MatterContract.Columns.BILLABLE + " INTEGER DEFAULT 1,"
+                + MatterContract.Columns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + MatterContract.Columns.ID + " INTEGER UNIQUE, "
+                + MatterContract.Columns.UPDATED_AT + " TEXT, "
+                + MatterContract.Columns.CREATED_AT + " TEXT, "
+                + MatterContract.Columns.DESCRIPTION + " TEXT, "
+                + MatterContract.Columns.DISPLAY_NUMBER + " TEXT, "
+                + MatterContract.Columns.STATUS + " TEXT, "
+                + MatterContract.Columns.PENDING_DATE + " TEXT, "
+                + MatterContract.Columns.OPEN_DATE + " TEXT, "
+                + MatterContract.Columns.CLOSE_DATE + " TEXT, "
+                + MatterContract.Columns.BILLABLE + " INTEGER DEFAULT 1"
             + ");";
 
     public DatabaseHelper(Context context) {
